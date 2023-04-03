97f9093a545fa51b50762ec5beeb066bdc4e772e
TiboMertens <tibomertens25@gmail.com>
2023-04-04 00:49:37 +0200

    fix: fixed a bug where the same error message would display for all input fields at the same time

--- signup.php
+++ signup.php
@@ -9,9 +9,23 @@ if (!empty($_POST)) {
     try {
         // create a new user
         $user = new User();
-        $user->setUsername($_POST['username']);
-        $user->setEmail($_POST['email']);
-        $user->setPassword($_POST['password']);
+        try {
+            $user->setUsername($_POST['username']);
+        } catch (Throwable $e) {
+            $usernameError = $e->getMessage();
+        }
+        
+        try {
+            $user->setEmail($_POST['email']);
+        } catch (Throwable $e) {
+            $emailError = $e->getMessage();
+        }
+        
+        try {
+            $user->setPassword($_POST['password']);
+        } catch (Throwable $e) {
+            $passwordError = $e->getMessage();
+        }
 
         // generate a random token  (32 characters)
         $user->setToken(bin2hex(openssl_random_pseudo_bytes(32)));
@@ -47,26 +61,26 @@ if (!empty($_POST)) {
                     <div class="w-30">
                         <div class="mb-4">
                             <label class="block font-bold mb-0.5" for="Email">Email</label>
-                            <input class="w-30 lg:w-55 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] <?php echo isset($error) ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="text" name="email">
+                            <input class="w-30 lg:w-55 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] <?php echo isset($emailError) ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="text" name="email">
                             <!-- if there is an error, show it -->
-                            <?php if (isset($error)) : ?>
-                                <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
+                            <?php if (isset($emailError)) : ?>
+                                <p class="text-red-500 text-xs italic"><?php echo $emailError; ?></p>
                             <?php endif; ?>
                         </div>
                         <div class="mb-4">
                             <label class="block font-bold mb-0.5" for="Username">Username</label>
-                            <input class="w-30 lg:w-50 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] <?php echo isset($error) ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="text" name="username" style="height:17px">
+                            <input class="w-30 lg:w-50 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] <?php echo isset($usernameError) ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="text" name="username" style="height:17px">
                             <!-- if there is an error, show it -->
-                            <?php if (isset($error)) : ?>
-                                <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
+                            <?php if (isset($usernameError)) : ?>
+                                <p class="text-red-500 text-xs italic"><?php echo $usernameError; ?></p>
                             <?php endif; ?>
                         </div>
                         <div class="mb-4">
                             <label class="block font-bold mb-0.5" for="Password">Password <span style="font-size: 7.65px; font-style: italic;">*a minimum of 5 characters</span></label>
-                            <input class="w-30 lg:w-50 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] <?php echo isset($error) ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="password" name="password" style="height:17px">
+                            <input class="w-30 lg:w-50 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] <?php echo isset($passwordError) ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="password" name="password" style="height:17px">
                             <!-- if there is an error, show it -->
-                            <?php if (isset($error)) : ?>
-                                <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
+                            <?php if (isset($passwordError)) : ?>
+                                <p class="text-red-500 text-xs italic" style="width: 200px;"><?php echo $passwordError; ?></p>
                             <?php endif; ?>
                         </div>
                         <div class="w-15"><button class="p-0 py-2 mt-2 mb-10 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded" type="submit" name="submit" style="padding-left: 4.65rem; padding-right:4.65rem">Sign up</button></div>
