d2bc232487fc56b31727b6c417ea5ab2af96425f
TiboMertens <tibomertens25@gmail.com>
2023-04-06 17:41:50 +0200

    fix: fixed a bug where not logged in user were not taken into account when loading non approved prompts

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ include_once("bootstrap.php");
 //start the session
 session_start();
 
-//check if user is an admin
-$user = new User();
-$isModerator = $user->isModerator($_SESSION['id']['id']);
+if (isset($_SESSION['loggedin'])) {
+    //check if user is an admin
+    $user = new User();
+    $isModerator = $user->isModerator($_SESSION['id']['id']);
 
-if ($isModerator) {
-    // new Moderator();
-    //get 15 images to approve
-    $imagesToApprove = Prompt::get15ToApproveImages();
+    if ($isModerator) {
+        // new Moderator();
+        //get 15 images to approve
+        $imagesToApprove = Prompt::get15ToApproveImages();
+    }
 }
 
 ?>
@@ -116,22 +118,24 @@ if ($isModerator) {
     </div>
 
     <main>
-        <?php if ($isModerator) : ?>
-            <section>
-                <h1 class="font-bold text-[24px] text-white ml-2 mb-2">Need approval <a href="showcase.php" class="text-[12px] text-blue-600 hover:text-blue-700 hover:text-[14px]">Expand<i class="fa-solid fa-arrow-right pl-1"></i></a></h1>
-                <div class="flex overflow-x-auto">
-                    <div class=" flex flex-shrink-0">
-                        <?php foreach ($imagesToApprove as $imageToApprove) : ?>
-                            <a href="#">
-                                <img src="<?php echo $imageToApprove['image_url']; ?>" alt="prompt">
-                            </a>
-                        <?php endforeach; ?>
-                        <div class="pt-20 mt-2 px-10">
-                            <a href="showcase.php" class="text-blue-600 hover:text-blue-700 font-bold underline">View all</a>
+        <?php if (isset($_SESSION['loggedin'])) : ?>
+            <?php if ($isModerator) : ?>
+                <section>
+                    <h1 class="font-bold text-[24px] text-white ml-2 mb-2">Need approval <a href="showcase.php" class="text-[12px] text-blue-600 hover:text-blue-700 hover:text-[14px]">Expand<i class="fa-solid fa-arrow-right pl-1"></i></a></h1>
+                    <div class="flex overflow-x-auto">
+                        <div class=" flex flex-shrink-0">
+                            <?php foreach ($imagesToApprove as $imageToApprove) : ?>
+                                <a href="#">
+                                    <img src="<?php echo $imageToApprove['image_url']; ?>" alt="prompt">
+                                </a>
+                            <?php endforeach; ?>
+                            <div class="pt-20 mt-2 px-10">
+                                <a href="showcase.php" class="text-blue-600 hover:text-blue-700 font-bold underline">View all</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            <?php endif; ?>
         <?php endif; ?>
         <!-- nieuwe prompts worden chronologisch getoond - gebruik AJAX infinite scroll(check voorbeeld feature Tibo wanneer deze af is && check Joris zijn video's) - feature britt -->
         <section></section>
