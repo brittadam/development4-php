759672c8313c714d9ca1323cda150ba76d9744bb
TiboMertens <tibomertens25@gmail.com>
2023-04-07 23:33:31 +0200

    chore: added security

--- promptDetails.php
+++ promptDetails.php
@@ -1,42 +1,51 @@
-<?php 
+<?php
 include_once("bootstrap.php");
 //DO NOT FORGET XSS PROTECTION
 
 //get id from url
-$prompt_id = $_GET['id'];
-$prompt = new Prompt();
-$prompt->setId($prompt_id);
+if (isset($_GET['id'])) {
+    $prompt_id = $_GET['id'];
+    $prompt = new Prompt();
+    $prompt->setId($prompt_id);
 
-//get prompt details
-$promptDetails = $prompt->getPromptDetails();
+    //get prompt details
+    $promptDetails = $prompt->getPromptDetails();
 
-//get data
-$title = $promptDetails['title'];
-$description = $promptDetails['description'];
-$cover_url = $promptDetails['cover_url'];
-$image2 = $promptDetails['image_url2'];
-$image3 = $promptDetails['image_url3'];
-$tstamp = $promptDetails['tstamp'];
-$price = $promptDetails['price'];
+    //get data
+    $title = $promptDetails['title'];
+    $description = $promptDetails['description'];
+    $cover_url = $promptDetails['cover_url'];
+    $image2 = $promptDetails['image_url2'];
+    $image3 = $promptDetails['image_url3'];
+    $tstamp = $promptDetails['tstamp'];
+    $price = $promptDetails['price'];
 
-//show data
-echo htmlspecialchars($title);
-echo htmlspecialchars($description);
-echo htmlspecialchars($cover_url);
-echo htmlspecialchars($image2);
-echo htmlspecialchars($image3);
-echo htmlspecialchars($tstamp);
-echo htmlspecialchars($price);
-//TODO: get user details
-// $authorID = $promptDetails['user_id'];
-// $user = new User(); 
-
-if ($_GET['approve'] === "true"){
-    $prompt->approvePrompt();
+    //show data
+    echo htmlspecialchars($title);
+    echo htmlspecialchars($description);
+    echo htmlspecialchars($cover_url);
+    echo htmlspecialchars($image2);
+    echo htmlspecialchars($image3);
+    echo htmlspecialchars($tstamp);
+    echo htmlspecialchars($price);
+    //TODO: get user details
+    // $authorID = $promptDetails['user_id'];
+    // $user = new User(); 
+} else {
+    $error = "No prompt id provided";
 }
 
-?><!DOCTYPE html>
+if (isset($_GET['approve'])) {
+    if ($_GET['approve'] === "true") {
+        $prompt->approvePrompt();
+    }
+}
+
+
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -44,9 +53,18 @@ if ($_GET['approve'] === "true"){
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Details</title>
 </head>
+
 <body>
-    <div>
-        <a href="promptDetails.php?id=<?php echo $prompt_id ?>&approve=true">Approve prompt</a>
-    </div>
+    <?php if (isset($_GET['approve'])) : ?>
+        <div>
+            <a href="promptDetails.php?id=<?php echo $prompt_id ?>&approve=true">Approve prompt</a>
+        </div>
+    <?php endif ?>
+    <?php if (isset($error)) : ?>
+        <div>
+            <p><?php echo $error ?></p>
+        </div>
+    <?php endif ?>
 </body>
+
 </html>
