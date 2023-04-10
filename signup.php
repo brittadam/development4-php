7cc15c3da26f45beff51b56de2098c03ea104eb4
TiboMertens <tibomertens25@gmail.com>
2023-04-10 03:54:13 +0200

    refactor: secured API key

--- signup.php
+++ signup.php
@@ -3,8 +3,11 @@ require_once 'vendor/autoload.php';
 include_once(__DIR__ . "/classes/User.php");
 include_once(__DIR__ . "/classes/Db.php");
 
-//TODO:needs to be secured
-apache_setenv('SENDGRID_API_KEY', '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');
+$config = parse_ini_file('config/config.ini', true);
+
+$key = $config[' keys ']['SENDGRID_API_KEY'];
+
+apache_setenv('SENDGRID_API_KEY', $key);
 
 if (!empty($_POST)) {
     try {
