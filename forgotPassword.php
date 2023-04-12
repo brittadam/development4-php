8a88ea5a251395c3e6fc1bea9d7f3bdf4566019c
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-04-12 20:39:12 +0200

    style: added front-end to forgotPassword.php

--- forgotPassword.php
+++ forgotPassword.php
@@ -32,13 +32,28 @@ if(!empty($_POST)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>forgot password</title>
 </head>
-<body>
-    <h1>reset password</h1>
-    <form action="" method="post">
-        <input type="text" name="email">
-        <button>send reset mail</button>
+<body class="bg-cover" style="background-image: url('images/signup-image.jpg')">
+<form action="" method="post">
+    
+    <div class="absolute inset-0 flex items-center justify-center">
+        <div class="bg-[#EAEAEA] w-1/2 md:w-1/3 xl:w-1/4 mx-auto my-auto rounded">
+            <h2 class="text-center pt-10 pb-7 text-2xl md:text-3xl font-bold mx-auto">Forgot password?</h2>
+            <div class="grid justify-items-center">
+                <div class="w-30">
+                    <div class="mb-4">
+                        <label class="block font-bold mb-0.5" for="email">Email</label>
+                        <input class="w-full lg:w-55 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] " style="height: 35px; font-size:1rem;" type="text" name="email">
+                    </div>
+                    <div class="flex flex-col items-center mb-10">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2" style="padding-left: 4rem; padding-right: 4rem;">Send mail</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </form>
 </body>
 </html>
