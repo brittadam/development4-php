9843659ddb764160b539b76f181bba9ea914e7b6
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-04-12 21:30:22 +0200

    style: added front-end to resetPassword.php

--- resetPassword.php
+++ resetPassword.php
@@ -27,13 +27,29 @@ if(isset($_GET['token'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
-    <h1>reset</h1>
-    <form action="" method="post">
-        <input type="password" name="password">
-        <button>reset</button>
+<body class="bg-cover" style="background-image: url('images/signup-image.jpg')">
+<form action="" method="post">   
+    <div class="absolute inset-0 flex items-center justify-center">
+        <div class="bg-[#EAEAEA] w-1/2 md:w-1/3 xl:w-1/4 mx-auto my-auto rounded">
+            <h2 class="text-center pt-10 pb-7 text-2xl md:text-3xl font-bold mx-auto">Reset password</h2>
+            <div class="grid justify-items-center">
+                <div class="w-30">
+                    <div class="mb-4">
+                        <label class="block font-bold mb-0.5" for="newPassword">New Password</label>
+                        <input class="w-full lg:w-55 px-3 py-2 border-2 rounded hover:border-[#143DF1] active:border-[#143DF1] " style="height: 35px; font-size:1rem;" type="password" name="password">
+                    </div>
+                    <div class="flex flex-col items-center mb-10">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2" style="padding-left: 5rem; padding-right: 5rem;">Reset</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </form>
+    
 </body>
 </html>
