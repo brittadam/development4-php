5a372b15e28fbfee1af695559c9236108cb78a41
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-04-22 21:03:36 +0200

    style: upload a prompt style added

--- js/tagInput.js
+++ js/tagInput.js
@@ -10,9 +10,11 @@ addTagButton.addEventListener("click", function(e) {
     newTagInput.setAttribute("type", "text");
     newTagInput.setAttribute("name", `tag${tagCount + 1}`);
     newTagInput.setAttribute("id", `tag${tagCount + 1}`);
+    newTagInput.setAttribute("class","w-full px-3 py-2 border-[3px] rounded hover:border-[#A25AFB] active:border-[#A25AFB]");
     const newTagLabel = document.createElement("label");
     newTagLabel.setAttribute("for", `tag${tagCount + 1}`);
     newTagLabel.innerText = `tag ${tagCount + 1}`;
+    newTagLabel.setAttribute("class","block font-bold mb-0.5 text-white");
     tagContainer.appendChild(newTagLabel);
     tagContainer.appendChild(newTagInput);
     tagCount++;
@@ -20,3 +22,34 @@ addTagButton.addEventListener("click", function(e) {
     addTagButton.disabled = true;
   }
 });
+
+function previewFile() {
+  var preview = document.getElementById('preview');
+  var file = document.getElementById('mainImage').files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = function() {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "<?php echo $mainImage ?>";
+  }
+}
+function previewFileOverview() {
+  var preview = document.getElementById('previewOverview');
+  var file = document.getElementById('overviewImage').files[0];
+  var reader = new FileReader();
+
+  reader.onloadend = function() {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = "<?php echo $overviewImage ?>";
+  }
+}
