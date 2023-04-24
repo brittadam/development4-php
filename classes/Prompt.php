0b1fa849669a52f3a6768e4a0524736b9d6d5864
TiboMertens <tibomertens25@gmail.com>
2023-04-24 21:06:33 +0200

    fix: only approved prompts get displayed on profile

--- classes/Prompt.php
+++ classes/Prompt.php
@@ -203,7 +203,7 @@ class prompt
     public static function getPromptsByUser($user_id)
     {
         $conn = Db::getInstance();
-        $statement = $conn->prepare("SELECT * FROM prompts WHERE user_id = :user_id");
+        $statement = $conn->prepare("SELECT * FROM prompts WHERE user_id = :user_id AND is_approved = 1");
         $statement->bindValue(":user_id", $user_id);
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
