6f754087c13b7a3116c2662d36851ef5e7260768
brittadam <britt.adam@hotmail.com>
2023-05-09 12:24:52 +0200

    fix: the errors in changepassword are more visible

--- changePassword.php
+++ changePassword.php
@@ -8,26 +8,53 @@ if (isset($_SESSION["loggedin"])) {
             $id = $_SESSION['id'];
             $password = $_POST['password'];
             $newPassword = $_POST['newPassword'];
+
             //create a new user object
             $user = new \Promptopolis\Framework\User();
             $user->setId($id);
-            $result = $user->canChangePassword($password);
+           
 
-            if ($result) {
-                $user->setPassword($newPassword);
-                $user->changePassword();
-                session_destroy();
-                header("Location: login.php");
-            } else {
-                throw new Exception("Incorrect old password.");
+            try {
+                $user->canChangePassword($password);
+            } catch (\Throwable $th) {
+                $oldPasswordError = $th->getMessage();
             }
+
+                try {
+                    $user->setPassword($newPassword);
+                                    
+                $result = $user->changePassword();
+                } catch (\Throwable $th) {
+                    $newPasswordError = $th->getMessage();
+                }
+
+                if (!empty($result)) {
+                    header("Location: profile.php");
+                    }
+
+            
         } catch (Throwable $e) {
             $error = $e->getMessage();
         }
+
+
+        if (isset($oldPasswordError)) {
+            $border = "border-red-500";
+        } else {
+            $border = "border-white";
+        }
+        
+        if (isset($newPasswordError)) {
+            $newborder = "border-red-500";
+        } else {
+            $newborder = "border-white";
+        }
+        
     }
 } else {
     header("Location: login.php");
 }
+
 
 ?>
 <!DOCTYPE html>
@@ -52,19 +79,23 @@ if (isset($_SESSION["loggedin"])) {
                         <div class="mb-4">
                             <label class="block font-bold mb-0.5 text-white" for="password">Current password</label>
                             <div class="flex flex-col items-end">
-                                <input class="w-full lg:w-50 px-3 py-2 border-[3px] border-white rounded hover:border-[#A25AFB] active:border-[#A25AFB] <?php echo isset($error) && ($error === 'Incorrect password.') ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="password" name="password">
+                                <input class=" w-full lg:w-50 px-3 py-2 border-[3px] <?php echo $border ?> rounded hover:border-[#A25AFB] active:border-[#A25AFB]" style="height: 35px; font-size:1rem;" type="password" name="password">
                             </div>
                         </div>
                         <div class="mb-4">
                             <label class="block font-bold mb-0.5 text-white" for="password">New password</label>
                             <div class="flex flex-col items-end">
-                                <input class="w-full lg:w-50 px-3 py-2 border-[3px] border-white rounded hover:border-[#A25AFB] active:border-[#A25AFB] <?php echo isset($error) && ($error === 'Incorrect password.') ? 'border-red-500' : ''; ?>" style="height: 35px; font-size:1rem;" type="password" name="newPassword">
+                            <input class="w-full lg:w-50 px-3 py-2 border-[3px] <?php echo isset($newPasswordError) ? 'border-red-500' : 'border-white'; ?> rounded hover:border-[#A25AFB] active:border-[#A25AFB]" style="height: 35px; font-size:1rem;" type="password" name="newPassword">
                             </div>
                         </div>
                         <!-- If there is an error, show it -->
-                        <?php if (isset($error)) : ?>
-                            <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
+                        <?php if (isset($oldPasswordError)) : ?>
+                            <p class="text-red-500 text-xs italic"><?php echo $oldPasswordError; ?></p>
                         <?php endif; ?>
+                        <?php if (isset($newPasswordError)) : ?>
+                            <p class="text-red-500 text-xs italic"><?php echo $newPasswordError; ?></p>
+                        <?php endif; ?>
+
                         <div class="flex flex-col items-center mb-10">
                             <button class="bg-[#BB86FC] hover:bg-[#A25AFB] text-white font-bold py-2 px-4 rounded mb-2" style="padding-left: 5rem; padding-right: 5rem;">Change password</button>
                         </div>
